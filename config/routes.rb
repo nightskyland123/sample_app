ca321b352d4c489f0c1ff5e636ea8b3691e70bc5
nightskyland123 <nightskyland123@yahoo.co.id>
2015-07-22 10:24:14 +0800

    add rspec for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :microposts
+  root 'sessions#new'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  get '/signup', to: "users#new"
+  get '/signin', to: "sessions#new"
+  delete '/signout', to: "sessions#destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
